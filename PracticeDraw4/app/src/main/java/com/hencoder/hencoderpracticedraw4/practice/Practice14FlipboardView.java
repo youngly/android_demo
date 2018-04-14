b3ea1eaa6236f0eb03ca462888372e79b289dc2a
yanglei <youngly.yang@gometech.com.cn>
2018-04-14 14:43:35 +0800

    custom view lesson 4

---
+++ PracticeDraw4/app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
@@ -0,0 +1,99 @@
+package com.hencoder.hencoderpracticedraw4.practice;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import com.hencoder.hencoderpracticedraw4.R;
+
+public class Practice14FlipboardView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    Camera camera = new Camera();
+    int degree;
+    ObjectAnimator animator = ObjectAnimator.ofInt(this, "degree", 0, 180);
+
+    public Practice14FlipboardView(Context context) {
+        super(context);
+    }
+
+    public Practice14FlipboardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice14FlipboardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+
+        animator.setDuration(2500);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        animator.start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        animator.end();
+    }
+
+    @SuppressWarnings("unused")
+    public void setDegree(int degree) {
+        this.degree = degree;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+        int x = centerX - bitmapWidth / 2;
+        int y = centerY - bitmapHeight / 2;
+
+        // 绘制上半部分
+        canvas.save();
+        canvas.clipRect(0, 0, getRight(), centerY);
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+        canvas.save();
+
+        camera.save();
+        camera.rotateX(degree);
+        canvas.translate(centerX, centerY);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-centerX, -centerY);
+        camera.restore();
+
+        canvas.clipRect(0, centerY, getRight(), getBottom());
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.drawText("hello", 0, 5, 0, 10, paint);
+        canvas.restore();
+    }
+}
